cfbfa3dc68d05033992dd162c05a028274c1d292
Max <max.gottschalk@mailbox.org>
2023-12-18 18:21:34 +0100

    add medusa dev option

--- app/src/lib/server/medusa.ts
+++ app/src/lib/server/medusa.ts
@@ -1,21 +1,25 @@
 import Medusa from '@medusajs/medusa-js';
 
+import { dev } from '$app/environment';
 import { MEDUSA_API_KEY, MEDUSA_BACKEND_URL } from '$env/static/private';
 import { MedusaClient } from 'sveltekit-medusa-client';
 
-export const medusa_sveltekit_client = new MedusaClient(MEDUSA_BACKEND_URL, {
-	retry: 0,
-	persistentCart: true,
-	logLevel: 'silent',
-	logFormat: 'json',
-	headers: {
-		'Content-Type': 'application/json',
-		Accept: 'application/json'
+export const medusa_sveltekit_client = new MedusaClient(
+	dev ? 'http://localhost:9000' : MEDUSA_BACKEND_URL,
+	{
+		retry: 0,
+		persistentCart: true,
+		logLevel: 'silent',
+		logFormat: 'json',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		}
 	}
-});
+);
 
 export const medusa_client = new Medusa({
-	baseUrl: MEDUSA_BACKEND_URL,
+	baseUrl: dev ? 'http://localhost:9000' : MEDUSA_BACKEND_URL,
 	maxRetries: 3,
 	apiKey: MEDUSA_API_KEY
 });
