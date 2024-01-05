5cfcb16611b7cdebbf80861b8b7666dc5ba2ace5
Max <max.gottschalk@mailbox.org>
2024-01-05 15:40:20 +0100

    Refactor profile layout component

--- app/src/routes/(app)/profile/+layout.svelte
+++ app/src/routes/(app)/profile/+layout.svelte
@@ -1,13 +1,5 @@
-<script lang="ts">
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-</script>
-
 <div class="noise bg-gray-50">
-	<div
-		class="max-w-layout border-secondary mx-4 my-4 rounded-md border bg-white px-6 py-6 shadow-[5px_5px_0px_0px_theme(colors.secondary.DEFAULT)]"
-	>
+	<div class="max-w-layout bg-white px-4 py-8">
 		<slot />
 	</div>
 </div>
