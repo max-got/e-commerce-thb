b275eb2d61b775581ba7c4d09bd7a43a7a99ed3a
Max <max.gottschalk@mailbox.org>
2024-01-06 17:31:24 +0100

    Update featured_products type in FeaturedProducts.svelte

--- app/src/lib/components/products/FeaturedProducts.svelte
+++ app/src/lib/components/products/FeaturedProducts.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import type { PricedProduct } from '@medusajs/types/';
-	export let featured_products: PricedProduct[] = [];
+	import type { Product } from '@medusajs/medusa';
+	export let featured_products: Product[] = [];
 
 	import ArrowRight from '~icons/ph/arrow-right';
 </script>
@@ -17,7 +17,8 @@
 			<h2 class="text-xl font-bold sm:text-3xl">Entdecke unsere Produkte</h2>
 
 			<p class="mx-auto mt-4 max-w-md text-white">
-				Richte deinen Blick auf die Zukunft. Mit unseren Produkten bist du für jedes Wetter gewappnet.
+				Richte deinen Blick auf die Zukunft. Mit unseren Produkten bist du für jedes Wetter
+				gewappnet.
 			</p>
 		</header>
 
@@ -51,65 +52,6 @@
 					</a>
 				</li>
 			{/each}
-			<!-- <li>
-				<a href="/" class=" group relative block">
-					<img
-						src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-						alt=""
-						class="aspect-square w-full rounded-lg object-cover transition duration-500 group-hover:opacity-90"
-					/>
-
-					<div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-						<h3 class="text-xl font-medium text-white">Casual Trainers</h3>
-
-						<span
-							class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
-						>
-							Shop Now
-						</span>
-					</div>
-				</a>
-			</li>
-
-			<li>
-				<a href="/" class="group relative block">
-					<img
-						src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-						alt=""
-						class="aspect-square h-full w-full rounded-lg object-cover transition duration-500 group-hover:opacity-90"
-					/>
-
-					<div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-						<h3 class="text-xl font-medium text-white">Winter Jumpers</h3>
-
-						<span
-							class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
-						>
-							Shop Now
-						</span>
-					</div>
-				</a>
-			</li>
-
-			<li class="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-				<a href="/" class="group relative block h-full">
-					<img
-						src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
-						alt=""
-						class="aspect-square h-full w-full rounded-lg object-cover transition duration-500 group-hover:opacity-90"
-					/>
-
-					<div class="absolute inset-0 flex flex-col items-start justify-end p-6">
-						<h3 class="text-xl font-medium text-white">Skinny Jeans Blue</h3>
-
-						<span
-							class="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
-						>
-							Shop Now
-						</span>
-					</div>
-				</a>
-			</li> -->
 		</ul>
 	</div>
 </section>
