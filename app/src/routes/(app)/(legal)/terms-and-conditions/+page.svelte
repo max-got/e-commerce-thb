26951650679c8e7bbe615dec5f967a6a2defb157
Max <max.gottschalk@mailbox.org>
2024-01-06 17:49:39 +0100

    Add terms and conditions page content

--- app/src/routes/(app)/(legal)/terms-and-conditions/+page.svelte
+++ app/src/routes/(app)/(legal)/terms-and-conditions/+page.svelte
@@ -1,1 +1,169 @@
+<h1>Allgemeine Geschäftsbedingungen</h1>
 
+<ol>
+	<li>Geltungsbereich</li>
+	<li>Vertragspartner</li>
+	<li>Vertragsschluss</li>
+	<li>Widerrufsrecht</li>
+	<li>Preise und Versandkosten</li>
+	<li>Lieferung</li>
+	<li>Zahlung</li>
+	<li>Eigentumsvorbehalt</li>
+	<li>Sachmängelgewährleistung</li>
+	<li>Streitbeilegung</li>
+</ol>
+
+<h2>Geltungsbereich</h2>
+<p>
+	Für alle Lieferungen von Zenit GmbH (nachfolgend ZenitShop) an Verbraucher gelten diese
+	Allgemeinen Geschäftsbedingungen (AGB). Verbraucher ist jede natürliche Person, die ein
+	Rechtsgeschäft zu einem Zwecke abschließt, der überwiegend weder ihrer gewerblichen noch ihrer
+	selbstständigen beruflichen Tätigkeit zugerechnet werden kann.
+</p>
+
+<h2>Vertragsschluss</h2>
+<p>
+	Der Kaufvertrag kommt zustande mit Zenit GmbH., <br />
+	Inhaber: Jonas Bethwell, <br />
+	Demostr. 1, <br />
+	12345 Demostadt, <br />
+	Handelsregister: Amtsgericht Demostadt, HRA 12345.
+</p>
+
+<h2>Vertragsschluss</h2>
+<p>
+	3.1. Die Darstellung der Produkte im Online-Shop stellt kein rechtlich bindendes An-gebot, sondern
+	nur eine Aufforderung zur Bestellung dar.
+</p>
+<p>
+	3.2. Durch Anklicken des Buttons [Kaufen/kostenpflichtig bestellen] geben Sie eine verbindliche
+	Bestellung der auf der Bestellseite aufgelisteten Waren ab. Ihr Kauf-vertrag kommt zustande, wenn
+	wir Ihre Bestellung durch eine Auftragsbestätigung per E-Mail unmittelbar nach dem Erhalt Ihrer
+	Bestellung annehmen.
+</p>
+
+<h2>Widerrufsrecht</h2>
+<p>
+	4.1. Wenn Sie Verbraucher sind (also eine natürliche Person, die die Bestellung zu einem Zweck
+	abgibt, der weder Ihrer gewerblichen oder selbständigen berufli-chen Tätigkeit zugerechnet werden
+	kann), steht Ihnen nach Maßgabe der gesetz-lichen Bestimmungen ein Widerrufsrecht zu.
+</p>
+
+<p>
+	4.2. Machen Sie als Verbraucher von Ihrem Widerrufsrecht nach Ziffer 4.1 Gebrauch, so haben Sie
+	die regelmäßigen Kosten der Rücksendung zu tragen.
+</p>
+<p>
+	4.3. Im Übrigen gelten für das Widerrufsrecht die Regelungen, die im Einzelnen wie-dergegeben sind
+	in der folgenden
+</p>
+
+<h2>Widerrufsbelehrung</h2>
+
+<h3>Widerrufsrecht</h3>
+<p>
+	Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen diesen Vertrag zu widerrufen.
+	Die Widerrufsfrist beträgt vierzehn Tage ab dem Tag an dem Sie oder ein von Ihnen benannter
+	Dritter, der nicht der Beförderer ist, die Waren in Besitz genom-men haben bzw. hat. Um Ihr
+	Widerrufsrecht auszuüben, müssen Sie uns [Namen des Unternehmers, Anschrift, Telefonnummer und
+	E-Mail Adresse eintragen] mittels einer eindeutigen Erklärung (zB ein mit der Post versandter
+	Brief oder eine E-Mail) über Ihren Ent-schluss, diesen Vertrag zu widerrufen, informieren. Sie
+	können dafür das beige-fügte Muster-Widerrufsformular verwenden, das jedoch nicht vorgeschrieben
+	ist. Sie können das Muster- Widerrufsformular oder eine andere eindeutige Erklärung auch auf
+	unserer Webseite (Internet-Adresse einfügen) elektronisch ausfüllen und übermitteln. Machen Sie
+	von dieser Möglichkeit Gebrauch, so werden wir Ihnen unverzüglich (zB per E-Mail) eine Bestätigung
+	über den Eingang eines solchen Widerrufs übermitteln. Zur Wahrung der Widerrufsfrist reicht es
+	aus, dass Sie die Mitteilung über die Aus-übung des Widerrufsrechts vor Ablauf der Widerrufsfrist
+	absenden.
+</p>
+
+<h3>Folgen des Widerrufs</h3>
+
+<p>
+	Wenn Sie diesen Vertrag widerrufen, haben wir Ihnen alle Zahlungen, die wir von Ihnen erhalten
+	haben, einschließlich der Lieferkosten (mit Ausnahme der zusätzli-chen Kosten, die sich daraus
+	ergeben, dass Sie eine andere Art der Lieferung als die von uns angebotene, günstigste
+	Standardlieferung gewählt haben), unverzüg-lich und spätestens binnen vierzehn Tagen ab dem Tag
+	zurückzuzahlen, an dem die Mitteilung über Ihren Widerruf dieses Vertrags bei uns eingegangen ist.
+	Für diese Rückzahlung verwenden wir dasselbe Zahlungsmittel, das Sie bei der ur-sprünglichen
+	Transaktion eingesetzt haben, es sei denn, mit Ihnen wurde aus-drücklich etwas anderes vereinbart;
+	in keinem Fall werden Ihnen wegen dieser Rückzahlung Entgelte berechnet. Wir können die
+	Rückzahlung verweigern, bis wir die Waren wieder zurückerhalten haben oder bis Sie den Nachweis
+	erbracht ha-ben, dass Sie die Waren zurückgesandt haben, je nachdem, welches der frühere Zeitpunkt
+	ist. Sie haben die Waren unverzüglich und in jedem Fall spätestens binnen vierzehn Tagen ab dem
+	Tag, an dem Sie uns über den Widerruf dieses Vertrages unterrich-ten, an uns oder an (hier sind
+	gegebenenfalls der Name und die Anschrift der von Ihnen zur Entgegennahme der Ware ermächtigten
+	Person einzufügen) zurückzu-senden oder zu übergeben. Die Frist ist gewahrt, wenn Sie die Waren
+	vor Ablauf der Frist von vierzehn Tagen absenden. Sie tragen die unmittelbaren Kosten der
+	Rücksendung der Waren. Sie müssen für einen etwaigen Wertverlust der Waren nur aufkommen, wenn
+	die-ser Wertverlust auf einen zur Prüfung der Beschaffenheit, Eigenschaften und Funktionsweise der
+	Waren nicht notwendigen Umgang mit ihnen zurückzuführen ist.
+</p>
+
+<p>
+	4.4. Über das Muster-Widerrufsformular informiert ZenitShop nach der gesetzlichen Regelung wie
+	folgt:
+</p>
+
+<h2>Muster-Widerrufsformular</h2>
+
+<p>
+	(Wenn Sie den Vertrag widerrufen wollen, dann füllen Sie bitte dieses Formular aus und senden Sie
+	es zurück.) <br />
+	-An ZenitGmbH, Demostr. 1, 12345 Demostadt,: <br />
+	–Hiermit widerrufe(n) ich/wir (*) den von mir/uns (*) abgeschlossenen Vertrag über den Kauf der folgenden
+	Waren (*)/die Erbringung der folgenden Dienstleistung (*) <br />
+	–Bestellt am (*)/erhalten am (*) <br />
+	–Name des/der Verbraucher(s) <br />
+	–Anschrift des/der Verbraucher(s) <br />
+	–Unterschrift des/der Verbraucher(s) (nur bei Mitteilung auf Papier) <br />
+	–Datum __________ <br />
+	(*) Unzutreffendes streichen
+</p>
+
+<h2>Preise und Versandkosten</h2>
+
+<p>
+	Die auf den Produktseiten genannten Preise enthalten die gesetzliche Mehrwert-steuer und sonstige
+	Preisbestandteile.
+</p>
+<p>
+	Zusätzlich zu den angegebenen Preisen berechnen wir für die Lieferung inner-halb Deutschlands
+	pauschal 5,90 Euro pro Bestellung. Die Versandkosten werden Ihnen auf den Produktseiten, im
+	Warenkorbsystem und auf der Bestellseite noch-mals deutlich mitgeteilt.
+</p>
+
+<h2>Lieferung</h2>
+<p>Die Lieferung erfolgt nur innerhalb Deutschlands mit DHL.</p>
+<p>
+	Die Lieferzeit beträgt bis zu 3 Tage. Auf eventuell abweichende Lieferzeiten weisen wir auf der
+	jeweiligen Produktseite hin.
+</p>
+
+<h2>Zahlung</h2>
+<p>Die Zahlung erfolgt wahlweise per Apple Pay, Paypal, Visa oder MasterCard.</p>
+
+<h2>Eigentumsvorbehalt</h2>
+<p>Bis zur vollständigen Zahlung bleibt die Ware unser Eigentum.</p>
+
+<h2>Sachmägelgewährleistung</h2>
+
+<p>
+	ZenitShop haftet für Sachmängel nach den hierfür geltenden gesetzlichen Vorschriften, insbesondere
+	§§ 434 ff Bürgerliches Gesetzbuch.
+</p>
+
+<h2>Streitbeilegung</h2>
+
+<p>
+	Die EU-Kommission hat eine Internetplattform zur Online-Beilegung von Streitigkeiten geschaffen.
+	Die Plattform dient als Anlaufstelle zur außergerichtlichen Bei-legung von Streitigkeiten
+	betreffend vertragliche Verpflichtungen, die aus Online-Kaufverträgen erwachsen. Nähere
+	Informationen sind unter dem folgenden Link verfügbar: http://ec.europa.eu/consumers/odr. Wir sind
+	zur Beilegung von Streitigkeiten mit Verbrauchern zur Teilnahme an einem Streitbeilegungsverfahren
+	vor einer Verbraucherschlichtungsstelle bereit oder gemäß GmbH verpflichtet. Die zuständige
+	Verbraucherschlichtungsstelle ist: Universalschlichtungsstelle des Bundes Zentrum für Schlichtung
+	e.V., Straßburger Straße 8, 77694 Kehl am Rhein, www.verbraucher-schlichter.de. Zur Beilegung der
+	genannten Streitigkeiten werden wir in einem Streitbeilegungsverfahren vor dieser Stelle
+	teilnehmen.
+</p>
