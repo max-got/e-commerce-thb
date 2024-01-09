f9f007ac299ccdb4abd65d51a2a41bbf4edeb72c
Max <max.gottschalk@mailbox.org>
2024-01-09 22:22:35 +0100

    Refactor product loading and error handling in +page.server.ts

--- app/src/routes/(app)/(shop)/s/p/[product_handle]/+page.server.ts
+++ app/src/routes/(app)/(shop)/s/p/[product_handle]/+page.server.ts
@@ -3,44 +3,45 @@ import { medusa_client } from '$lib/server/medusa';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
+const get_product = async ({ handle }: { handle: string }) => {
+	try {
+		const product = await medusa_client.products.list({
+			handle: handle,
+			region_id: MEDUSA_REGION_ID
+		});
+
+		return product.products[0];
+	} catch (e) {
+		//!TODO handle error
+		error(404, {
+			message: 'Not found'
+		});
+	}
+};
+
+const get_other_products = async ({ handle }: { handle: string }) => {
+	try {
+		const products = await medusa_client.products.list({
+			limit: 4,
+			region_id: MEDUSA_REGION_ID
+		});
+
+		products.products = products.products.filter((p) => p.handle !== handle);
+
+		return products.products;
+	} catch (e) {
+		//!TODO handle error
+		error(404, {
+			message: 'Not found'
+		});
+	}
+};
+
 export const load = (async ({ params }) => {
 	const { product_handle } = params;
 
-	const get_the_product = async ({ handle }: { handle: string }) => {
-		try {
-			const product = await medusa_client.products.list({
-				handle: handle,
-				region_id: MEDUSA_REGION_ID
-			});
-
-			return product.products[0];
-		} catch (e) {
-			//!TODO handle error
-			error(404, {
-				message: 'Not found'
-			});
-		}
-	};
-
-	const get_other_products = async ({ handle }: { handle: string }) => {
-		try {
-			const products = await medusa_client.products.list({
-				limit: 4
-			});
-
-			products.products = products.products.filter((p) => p.handle !== handle);
-
-			return products.products;
-		} catch (e) {
-			//!TODO handle error
-			error(404, {
-				message: 'Not found'
-			});
-		}
-	};
-
 	return {
 		other_products: get_other_products({ handle: product_handle }),
-		product: await get_the_product({ handle: product_handle })
+		product: await get_product({ handle: product_handle })
 	};
 }) satisfies PageServerLoad;
