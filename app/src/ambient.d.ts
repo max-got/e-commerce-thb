fde28cd3bdf672a6020051bca7e43c5c8cf62524
Max <max.gottschalk@mailbox.org>
2024-01-10 20:51:44 +0100

    Add ProductVariant interface to ambient.d.ts

--- app/src/ambient.d.ts
+++ app/src/ambient.d.ts
@@ -1,2 +1,14 @@
-
-
+declare module '@medusajs/medusa/dist/models/product-variant' {
+	declare interface ProductVariant {
+		metadata:
+			| {
+					gewicht: string;
+					streben: string;
+					spannweite: string;
+					groesse_geschlossen: string;
+					verschlussmechanismus: string;
+					herstellungsmaterialien: string;
+			  }
+			| { [key: string]: string | null };
+	}
+}
