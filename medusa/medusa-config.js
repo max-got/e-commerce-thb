e065c9e0526a4f2f69fa34ca1d7e66d4a8bd882a
Max <max.gottschalk@mailbox.org>
2024-01-11 12:33:35 +0100

    Refactor medusa-config.js to simplify environment file selection

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -1,26 +1,5 @@
 const dotenv = require('dotenv');
 
-let ENV_FILE_NAME = '';
-switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
-		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
-		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
-		break;
-	case 'development':
-	default:
-		ENV_FILE_NAME = '.env';
-		break;
-}
-
-try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
-} catch (e) {}
-
 const plugins = [
 	`medusa-fulfillment-manual`,
 	`medusa-payment-manual`,
